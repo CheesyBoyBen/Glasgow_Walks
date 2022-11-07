49ea76c65d443fcc531e3d69d768812706f4eaca
DESKTOP-OUAALTE\benja <benjaminlockhart@hotmail.co.uk>
2022-11-07 13:10:01 +0000

    Commenting all previous code
    The comments of both scripts is now fully up to date

--- Preproduction Prototype/Assets/Scripts/GPS_Location.cs
+++ Preproduction Prototype/Assets/Scripts/GPS_Location.cs
@@ -13,34 +13,37 @@ public class GPS_Location : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Input.gyro.enabled = true;
+        Input.gyro.enabled = true;      // Enable the use of the phones Gyroscope
 
+        // If the app has location services enabled
         if (Input.location.isEnabledByUser)
         {
-            StartCoroutine(GetLocation());
+            StartCoroutine(GetLocation());      // Call the GetLocation function
         }
     }
 
     private IEnumerator GetLocation()
     {
-        Input.location.Start();
+        Input.location.Start();     // Begin using the phones location services
+
+        // While the location services are initializing
         while (Input.location.status == LocationServiceStatus.Initializing)
         {
-            yield return new WaitForSeconds(0.5f);
+            yield return new WaitForSeconds(0.5f);      // Wait for half a second
         }
-        latitude = Input.location.lastData.latitude;
-        longitude = Input.location.lastData.longitude;
-        yield break;
+        latitude = Input.location.lastData.latitude;    // Set the latitude float to the value of the phones GPS latitude
+        longitude = Input.location.lastData.longitude;  // Set the longitude float to the value of the phones GPS longitude
+        yield break;    // End the function
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        latitude = Input.location.lastData.latitude;
-        longitude = Input.location.lastData.longitude;
-        attitude = Input.gyro.attitude.eulerAngles.z;
-        gpsText.text = "Lat: " + latitude + "\nLon: " + longitude + "\nAtt: " + attitude;
+        latitude = Input.location.lastData.latitude;    // Set the latitude float to the value of the phones GPS latitude
+        longitude = Input.location.lastData.longitude;  // Set the longitude float to the value of the phones GPS longitude
+        attitude = Input.gyro.attitude.eulerAngles.z;   // Set the attitude float to the value of the phones gyroscopic rotation around the x axis
+        gpsText.text = "Lat: " + latitude + "\nLon: " + longitude + "\nAtt: " + attitude;       // Display the latitude, longitude and attitude floats
 
     }
 
--- Preproduction Prototype/Assets/Scripts/ScrollandPinch.cs
+++ Preproduction Prototype/Assets/Scripts/ScrollandPinch.cs
@@ -19,9 +19,9 @@ public class ScrollandPinch : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rotType = false;
+        rotType = false;    // Set initial rotation type to be gyroscopic
 
-        Input.gyro.enabled = true;
+        Input.gyro.enabled = true;      // Enable the use of the phones gyroscope
 
     }
 
@@ -29,85 +29,82 @@ public class ScrollandPinch : MonoBehaviour
     void Update()
     {
         
-
+        // Count number of touch inputs
         switch (Input.touchCount)
         {
+            // If there is 1 touch registered
             case 1:
                 
+                // If the rotation type is manual
                 if (rotType)
-                {
-                    Touch touch = Input.GetTouch(0);
+                {                    
+                    Touch touch = Input.GetTouch(0);    // Get instance of touch
 
-                    /*if (touch.position.x > (touch.position - touch.deltaPosition).x)
-                    {
-                        cvc.m_XAxis.Value -= (touch.deltaPosition.magnitude * 0.05f);
-                    }
-                    else
-                    {
-                        cvc.m_XAxis.Value += (touch.deltaPosition.magnitude * 0.05f);
-                    }*/
+                    
+                    Vector2 touchPrevPos = touch.position - touch.deltaPosition;    // Find the position of the touch in the last update                    
+                    Vector2 playerPos = cam.WorldToScreenPoint(player.transform.position);      // Find the current position of the touch
 
-                    Vector2 touchPrevPos = touch.position - touch.deltaPosition;
-                    Vector2 playerPos = cam.WorldToScreenPoint(player.transform.position);
+                    float prevAngle = Mathf.Atan2(touchPrevPos.y - playerPos.y, touchPrevPos.x - playerPos.x);      // Find the angle of the last touch position ralative to the players on screen coords
+                    prevAngle = prevAngle * Mathf.Rad2Deg;      // Convert the previous angle to degrees from radians
+                    if (prevAngle < 0) { prevAngle += 360; }    // convert the previous angle from a range of -180 -> 180 to 0 -> 360
 
-                    float prevAngle = Mathf.Atan2(touchPrevPos.y - playerPos.y, touchPrevPos.x - playerPos.x);
-                    prevAngle = prevAngle * Mathf.Rad2Deg;
-                    if (prevAngle < 0) { prevAngle += 360; }
-
-                    float Angle = Mathf.Atan2(touch.position.y - playerPos.y, touch.position.x - playerPos.x);
-                    Angle = Angle * Mathf.Rad2Deg;
-                    if (Angle < 0) { Angle += 360; }
+                    float Angle = Mathf.Atan2(touch.position.y - playerPos.y, touch.position.x - playerPos.x);      // Find the angle of the current touch position ralative to the players on screen coords
+                    Angle = Angle * Mathf.Rad2Deg;      // Convert the current angle to degrees from radians
+                    if (Angle < 0) { Angle += 360; }    // convert the current angle from a range of -180 -> 180 to 0 -> 360
 
 
-                    if (((Angle - prevAngle) < 300) && ((Angle - prevAngle) > -300)) { cvc.m_XAxis.Value += (Angle - prevAngle) / 2; }
+                    // Ignore changes in the angle of over 300 degrees to account for the point where 360 goes to 0
+                    if (((Angle - prevAngle) < 300) && ((Angle - prevAngle) > -300)) { cvc.m_XAxis.Value += (Angle - prevAngle) / 2; }      // Add half of the change in angle to the cinemachine camera X axis
                                         
                 }
 
+                // If the rotation type is gyroscopic
                 if (!rotType)
                 {
-                    Debug.Log(Input.gyro.attitude.eulerAngles.x);
-                    cvc.m_XAxis.Value = (Input.gyro.attitude.eulerAngles.z - 180) * -1;
+                    cvc.m_XAxis.Value = (Input.gyro.attitude.eulerAngles.z - 180) * -1;     // Set the value of the cinemachine camera X axis to the gyroscopic rotaion of the phones z axis converted to a range of 0 -> 360
 
                 }
 
-                break;
+                break;  // End the current switch case
 
+            // If there is 2 touches registered
             case 2:
-                Touch touchZero = Input.GetTouch(0);
-                Touch touchOne = Input.GetTouch(1);
 
-                Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
-                Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+                Touch touchZero = Input.GetTouch(0);    // Get instance of first touch
+                Touch touchOne = Input.GetTouch(1);     // Get instance of second touch
 
-                float prevMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
-                float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
+                Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;    // Find the position of the first touch in the last update
+                Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;       // Find the position of the second touch in the last update
 
-                float difference = currentMagnitude - prevMagnitude;
+                float prevMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;       // Find the distance between the 2 touches in the previous update
+                float currentMagnitude = (touchZero.position - touchOne.position).magnitude;    // Find the current distance between the 2 touches
+
+                float difference = currentMagnitude - prevMagnitude;    // Calculate the change in distance between the 2 touchs from the last update to now
 
 
-                cvc.m_YAxis.Value -= (difference * 0.001f);
+                cvc.m_YAxis.Value -= (difference * 0.001f);     // Subtract the change in distance multiplied by a constant from the cinemachine cameras Y axis
 
-                break;
+                break;  // End the current switch case
         }
     }
 
     public void MoveZoom()
     {
+        // If the camera is currently fully zoomed out
         if (cvc.m_YAxis.Value == 1)
         {
-            cvc.m_YAxis.Value = 0;
+            cvc.m_YAxis.Value = 0;      // Set the camera to fully zoomed in
         }
+        // Otherwise if the camera is not fully zoomed out
         else
         {
-            cvc.m_YAxis.Value = 1;
-
+            cvc.m_YAxis.Value = 1;      // Set the camera to fully zoomed out
         }
     }
 
     public void SwitchRotType()
-    {        
-        if (rotType) { rotType = false; }
-        else if (!rotType) { rotType = true; }        
+    {
+        rotType = !rotType;     // Toggle between the rotation types      
     }
 
 
