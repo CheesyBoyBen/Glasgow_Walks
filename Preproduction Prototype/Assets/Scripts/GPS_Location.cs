f42879df83021bb5eb06151a13bc5c8f4660c69b
DESKTOP-OUAALTE\benja <benjaminlockhart@hotmail.co.uk>
2022-11-08 18:56:43 +0000

    Added Mapbox

--- Preproduction Prototype/Assets/Scripts/GPS_Location.cs
+++ Preproduction Prototype/Assets/Scripts/GPS_Location.cs
@@ -42,8 +42,8 @@ public class GPS_Location : MonoBehaviour
         {
             yield return new WaitForSeconds(0.5f);      // Wait for half a second
         }
-        latitude = Input.location.lastData.latitude;    // Set the latitude float to the value of the phones GPS latitude
-        longitude = Input.location.lastData.longitude;  // Set the longitude float to the value of the phones GPS longitude
+        //latitude = Input.location.lastData.latitude;    // Set the latitude float to the value of the phones GPS latitude
+        //longitude = Input.location.lastData.longitude;  // Set the longitude float to the value of the phones GPS longitude
         yield break;    // End the function
     }
 
@@ -52,8 +52,8 @@ public class GPS_Location : MonoBehaviour
     {
         if (Input.location.isEnabledByUser)
         {
-            latitude = Input.location.lastData.latitude;    // Set the latitude float to the value of the phones GPS latitude
-            longitude = Input.location.lastData.longitude;  // Set the longitude float to the value of the phones GPS longitude
+            //latitude = Input.location.lastData.latitude;    // Set the latitude float to the value of the phones GPS latitude
+            //longitude = Input.location.lastData.longitude;  // Set the longitude float to the value of the phones GPS longitude
             attitude = Input.gyro.attitude.eulerAngles.z;   // Set the attitude float to the value of the phones gyroscopic rotation around the x axis
             gpsText.text = "Lat: " + latitude + "\nLon: " + longitude + "\nAtt: " + attitude;       // Display the latitude, longitude and attitude floats
         }
