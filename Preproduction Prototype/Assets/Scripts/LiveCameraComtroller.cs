fcef08e9a04f84928fbb0731502eed75c951b2a5
DESKTOP-OUAALTE\benja <benjaminlockhart@hotmail.co.uk>
2022-11-23 16:49:12 +0000

    Live Camera now working
    -Added a LiveCam Plane
    -Swapped Cam to front facing
    - Added a cam button to switch cam on/off

---
+++ Preproduction Prototype/Assets/Scripts/LiveCameraComtroller.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiveCameraComtroller : MonoBehaviour
+{
+        public WebCamTexture mCamera = null;
+        public GameObject plane;
+        string frontCamName = null;
+
+    // Use this for initialization
+    void Start ()
+    {
+        Debug.Log ("Script has been started");
+        plane = GameObject.FindWithTag ("LiveCam");
+
+                
+
+        var webCamDevices = WebCamTexture.devices;
+        foreach (var camDevice in webCamDevices)
+        {
+            if (camDevice.isFrontFacing)
+            {
+                frontCamName = camDevice.name;
+                break;
+                
+            }
+
+        }
+        mCamera = new WebCamTexture(frontCamName);
+        plane.GetComponent<Renderer>().material.mainTexture = mCamera;
+        mCamera.Play ();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetActive()
+    {
+        if (plane.activeSelf)
+        {
+            plane.SetActive(false);
+        }
+        else
+        {
+            plane.SetActive(true);
+        }
+    }
+}
