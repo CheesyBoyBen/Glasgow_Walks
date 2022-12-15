0dbf15c86c9cbe8aca7b640019e2e736310d5db7
DESKTOP-OUAALTE\benja <benjaminlockhart@hotmail.co.uk>
2022-12-15 10:44:16 +0000

    final final push

--- Preproduction Prototype/Assets/Scripts/PlayerScript.cs
+++ Preproduction Prototype/Assets/Scripts/PlayerScript.cs
@@ -34,7 +34,7 @@ public class PlayerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        testCoord = false;
+        testCoord = true;
         Screen.orientation = ScreenOrientation.Portrait;
     }
 
